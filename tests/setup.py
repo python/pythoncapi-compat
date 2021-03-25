386a4ea0e661ff2131133186cc828ab2ebe9a169
Victor Stinner <vstinner@python.org>
2021-03-25 20:42:54 +0100

    Add Windows and macOS CI

--- tests/setup.py
+++ tests/setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
+import os.path
 
+
+SRC_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+
+# C compiler flags for GCC and clang
 CFLAGS = ['-Wall', '-Wextra', '-Werror']
 
 def main():
     from distutils.core import setup, Extension
 
+    cflags = ['-I' + SRC_DIR]
+    if os.name != "nt":
+        cflags.extend(CFLAGS)
+
     ext = Extension('test_pythoncapi_compat_cext',
                     sources=['test_pythoncapi_compat_cext.c'],
-                    extra_compile_args=CFLAGS)
+                    extra_compile_args=cflags)
 
     setup(name="test_pythoncapi_compat", ext_modules=[ext])
 
