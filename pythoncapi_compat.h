4678af46f6015a3b38d84498f6d5e165c9df2479
Victor Stinner <vstinner@python.org>
2023-12-15 03:58:37 +0100

    Add Py_HashPointer() (#83)

--- pythoncapi_compat.h
+++ pythoncapi_compat.h
@@ -1089,6 +1089,25 @@ PyDict_PopString(PyObject *dict, const char *key, PyObject **result)
 }
 #endif
 
+
+#if PY_VERSION_HEX < 0x030200A4
+// Python 3.2.0a4 added Py_hash_t type
+typedef Py_ssize_t Py_hash_t;
+#endif
+
+
+// gh-111545 added Py_HashPointer() to Python 3.13.0a3
+#if PY_VERSION_HEX < 0x030D00A3
+static inline Py_hash_t Py_HashPointer(const void *ptr)
+{
+#if PY_VERSION_HEX >= 0x030900A4 && !defined(PYPY_VERSION)
+    return _Py_HashPointer(ptr);
+#else
+    return _Py_HashPointer(_Py_CAST(void*, ptr));
+#endif
+}
+#endif
+
 #ifdef __cplusplus
 }
 #endif
