7820c6e093cb0db7695a7988cdded5d1c495a7a1
Victor Stinner <vstinner@python.org>
2024-02-20 15:04:17 +0100

    Use time.perf_counter_ns() on PyPy 3.7 and newer

--- pythoncapi_compat.h
+++ pythoncapi_compat.h
@@ -1129,11 +1129,42 @@ static inline int PyTime_PerfCounter(PyTime_t *result)
 {
 #if PY_VERSION_HEX >= 0x03070000 && !defined(PYPY_VERSION)
     return _PyTime_GetPerfCounterWithInfo(result, NULL);
-#else
-    // Cache time.perf_counter() function for best performance
+#elif PY_VERSION_HEX >= 0x03070000
+    // Call time.perf_counter_ns() and convert Python int object to PyTime_t.
+    // Cache time.perf_counter_ns() function for best performance.
     static PyObject *func = NULL;
     if (func == NULL) {
-        // Call time.perf_counter() and convert C double to PyTime_t
+        PyObject *mod = PyImport_ImportModule("time");
+        if (mod == NULL) {
+            return -1;
+        }
+
+        func = PyObject_GetAttrString(mod, "perf_counter_ns");
+        Py_DECREF(mod);
+        if (func == NULL) {
+            return -1;
+        }
+    }
+
+    PyObject *res = PyObject_CallNoArgs(func);
+    if (res == NULL) {
+        return -1;
+    }
+    long long value = PyLong_AsLongLong(res);
+    Py_DECREF(res);
+
+    if (value == -1 && PyErr_Occurred()) {
+        return -1;
+    }
+
+    Py_BUILD_ASSERT(sizeof(value) >= sizeof(PyTime_t));
+    *result = (PyTime_t)value;
+    return 0;
+#else
+    // Call time.perf_counter() and convert C double to PyTime_t.
+    // Cache time.perf_counter() function for best performance.
+    static PyObject *func = NULL;
+    if (func == NULL) {
         PyObject *mod = PyImport_ImportModule("time");
         if (mod == NULL) {
             return -1;
